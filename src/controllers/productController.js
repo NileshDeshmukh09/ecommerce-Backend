26fd16f270b3b3f48c4a0e2f2226b8c3c8352cb5
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2022-01-28 10:25:30 +0530

    Added Product Addition API

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -15,4 +15,21 @@ function listProducts(req, res) {
     })
 }
 
-module.exports = {listProducts};
+function addProduct(req, res){
+    let data = req.body;
+    if(data.name && data.price && data.description && data.categoryId && data.vendorId){
+        Product.addProduct(data , function(err, result){
+            if(err){
+                console.log(err);
+                return res.status(500).send({message: "Not Ok!"});
+            }
+            return res.status(200).send({
+                success:true,
+                msg: "Successfully Added Products!",
+                products : result
+            })
+        })
+    }
+}
+
+module.exports = {listProducts, addProduct};
