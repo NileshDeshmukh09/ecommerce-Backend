01c50d7c74b6bd6d619d8146753ba1edb61f6f1a
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2022-01-28 23:23:09 +0530

    Added Routes basced on API v1

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cors = require('cors');
 
 
-var indexRouter = require('./routes/api/v1/index');
-// var apiRouter = require('./routes/api');
+// var indexRouter = require('./routes/api/v1/index');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter); //whenever we will hit any URL starting with '/' then we will Invoke the IndexRouter
-// app.use('/api', apiRouter);
+// app.use('/', indexRouter); //whenever we will hit any URL starting with '/' then we will Invoke the IndexRouter
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
