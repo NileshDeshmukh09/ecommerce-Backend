1254142da92b80c035d553b9f1b2d62ad7cfe152
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2022-01-30 11:53:43 +0530

    Created singup

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 const categoryController = require("../../../src/controllers/categoryController");
 const productController = require("../../../src/controllers/productController");
 const orderDetailsController = require("../../../src/controllers/orderController");
+const userController = require("../../../src/controllers/userController");
 
 var router = express.Router();
 
@@ -9,5 +10,7 @@ router.post("/category/all", categoryController.listCategories);
 router.post("/product/all",productController.listProducts );
 router.post("/product/add",productController.addProduct );
 router.post("/order/details",orderDetailsController.getOrderDetails );
+router.post("/user/signup",userController.signup );
+
 
 module.exports = router;
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/products");
+const Product = require("../models/product");
 
 function listProducts(req, res) {
     let data = req.body;
